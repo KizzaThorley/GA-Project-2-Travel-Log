4cf59de382c402a49a275f7316ca822f7be8b579
kizza <kieran.thorley@gmail.com>
2024-05-24 13:32:22 +0100

    Initial folder setup
    -added dependencies
    -added initial user model
    - added travel and auth controllers
    -added middleware for session avaliable in all ejs
    - added home page
    - added connection to mongoDB
    -public folder for future css
    - views for future ejs

---
+++ models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const TravelSchema = new mongoose.Schema({ 
+    country: { type: String, required: true },
+    place: { type: String, required: true },
+     hasBeen: { type: Boolean, required: true},
+     review: { type: String, required: false},
+     rating: { type: Number, required: false},
+});
+
+const UserSchema = new mongoose.Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+     displayTravel: { type: Boolean, required: true},
+     destination: [TravelSchema],
+}); 
+
+const User = mongoose.model('User', UserSchema)
+
+module.exports = User;
